0a72a269834f36994abaa5d2fb132081949b148b
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-12-14 16:15:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function produceDrivingRange(range){
-  return function drivingRange(trip){
-    if(trip < range){
-      return true
-    } else {
-      return false
+  return function(endingBlock, startingBlock){
+    let start = parseInt(startingBlock)
+    let end = parseInt(endingBlock)
+    let delta = end - start
+    
+    if(delta > range){
+      return `${delta-range} blocks out of range`
+    } else{
+      return `within range by ${range-delta}`
     }
-  };
+  }
 }
