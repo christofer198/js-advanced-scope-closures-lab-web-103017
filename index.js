ab43f866a52770c3ade6e078115cc950554bcb58
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-12-14 16:25:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,13 @@ function produceTipCalculator(tip){
     return fare * tip
   }
 }
+
+function createDriver(){
+  let driverId = 0
+  return class {
+    constructor(name){
+      this.name = name
+      this.id = ++driverId
+    }
+  } 
+}
